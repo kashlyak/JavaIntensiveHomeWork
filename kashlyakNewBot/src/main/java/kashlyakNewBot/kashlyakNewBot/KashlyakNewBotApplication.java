d507791bbe2238f3e2fcd269eec1b94330283523
Alexey Kashlyak <lexa.april@gmail.com>
2020-11-18 16:02:55 +0300

    My telegram bot for ParseWeather and task from Andrey to add MySQL base, button for check BD

---
+++ kashlyakNewBot/src/main/java/kashlyakNewBot/kashlyakNewBot/KashlyakNewBotApplication.java
@@ -0,0 +1,18 @@
+package kashlyakNewBot.kashlyakNewBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+
+
+@SpringBootApplication
+public class KashlyakNewBotApplication {
+
+
+	public static void main(String[] args) {
+		ApiContextInitializer.init();
+		SpringApplication.run(KashlyakNewBotApplication.class, args);
+	}
+}
+
+
